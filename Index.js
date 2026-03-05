1ef7ed244692140215b023939fa25f6648c58208
sahasra674 <23409sg@hvhs.school.nz>
2026-03-06 09:19:10 +1300

    7

---
+++ Index.js
@@ -0,0 +1,31 @@
+/*******************************************************/
+function setup() {
+	
+
+  // Now the width and height of the canvas keep changing
+width = random(600,600);
+height = random(600,600);
+
+cnv = new Canvas(width, height);
+
+  // Rectangle sprite
+  box = new Sprite(200, 150, 120, 60);
+  box.color = 'orange';
+  box.rotationSpeed = 2;
+  box.vel.x = 2;
+}
+	
+/*******************************************************/
+// draw()
+/*******************************************************/
+function draw() {
+	 // Background colour
+  background('lightgrey');
+
+
+};
+
+
+/*******************************************************/
+//  END OF APP
+/*******************************************************/
