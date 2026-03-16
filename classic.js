d2e37f18aad74d8f681ab4c1b8c7a90f9ff92a15
sahasra674 <23409sg@hvhs.school.nz>
2026-03-17 11:35:16 +1300

    11

--- classic.js
+++ classic.js
@@ -1,12 +1,37 @@
+
+// setup()
+/*******************************************************/
+
+
+// Select the button by its ID
+const button = document.getElementById("myButton");
+
+// Add a click event listener
+button.addEventListener("click", () => {
+    alert("Hello! You clicked the button.");
+});
+
+
+
 function setup() {
 	console.log("setup: ");
 
-	cnv = new Canvas(500, 600);2
+    alert("connect Js");
+  
+cnv = new Canvas(600, 500);
+  
+// script.js
+console.log("JavaScript file is connected!");
 
-wallLH = new Sprite(0, height/2, 8, height, 'k');
+ // Create your sprite
+ box = new Sprite(200, 150, 120, 60,'d');
+  box.color = 'orange';
+
+  //wall
+wallLH  = new Sprite(0, height/2, 8, height, 'k');
 wallLH.color = 'black';
 
-wallRH = new Sprite(width, height/2, 8, height, 'k');
+wallRH  = new Sprite(width, height/2, 8, height, 'k');
 wallRH.color = 'black';
 
 wallTop = new Sprite(width/2, 0, width, 8, 'k');
@@ -14,45 +39,32 @@ wallTop.color = 'black';
 
 wallBot = new Sprite(width/2, height, width, 8, 'k');
 wallBot.color = 'black';
-
-
-ball_1 = new Sprite(width/2, height/2, 50, 'd');
-ball_1.color = 'cyan';
-ball_1.vel.y = 2 
-ball_1.vel.x = 2;
-ball_1.bounciness = 1;
-ball_1.friction = 0;
-ball_1.drag = 0;
-
-//alienGroup = new Group();
-
-for (i = 0; i < 5; i++) {
-  alien = new Sprite(i * 60 + 40, 20, 60, 30);
-  //alien.color = color(255, 203, 225);
-//alienGroup.add(alien);
-//alienGroup = new Group();
+ 
 }
-}
-
-
-
-
+ 
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
-	background('lightgrey');
 
-  // Register a callback:
-// if any alien in alienGroup collides with ball_1, call func2Call
-alienGroup.collides(ball_1, func2Call);
+  background('lightgrey');
 
-function func2Call(_alien, _ball_1) {
-
-// Delete the alien which was hit
-_alien.remove();
-
+  if (kb.pressing('left')) {
+box.vel.x = -5;
 }
+
+else if (kb.pressing ('right')) {
+box.vel.x = 5; 
+};
+
+if (kb.released('left')) {
+  box.vel.x = 0; 
+}
+
+if (kb.released('right')) {
+  box.vel.x = -0; 
+}
+  
 }
 
 /*******************************************************/
